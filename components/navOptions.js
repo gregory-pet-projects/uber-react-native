438e7249b6b9de3b26c4ab69853f75f3f9249194
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-20 12:30:53 +0300

    created nav options component

---
+++ components/navOptions.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const navOptions = () => {
+  return (
+    <View>
+      <Text>navOptions</Text>
+    </View>
+  );
+};
+
+export default navOptions;
