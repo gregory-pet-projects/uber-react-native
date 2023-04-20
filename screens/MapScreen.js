cb7d97abd328892ea653cfcaae8393735a53829f
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-20 16:19:46 +0300

    aded map screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,20 @@
 import { View, Text } from "react-native";
 import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { Map } from "../components";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const screens = [];
 
 const MapScreen = () => {
+  const Stack = createNativeStackNavigator();
   return (
     <View>
-      <Text>MapScreen</Text>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>{renderScreens(Stack, screens)}</Stack.Navigator>
+      </View>
     </View>
   );
 };
