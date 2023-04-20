8932a73c7e1ab5e0da930d21a2b0a9809cebc003
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-20 17:06:13 +0300

    added new imports

--- components/index.js
+++ components/index.js
@@ -1,4 +1,13 @@
 import Map from "./Map";
 import NavOptions from "./NavOptions";
+import NavigateCard from "./NavigateCard";
+import RideOptionsCard from "./RideOptionsCard";
+import GooglePlacesComponent from "./GooglePlacesComponent";
 
-export { NavOptions, Map };
+export {
+  NavOptions,
+  Map,
+  NavigateCard,
+  RideOptionsCard,
+  GooglePlacesComponent,
+};
