210165fe94e6ca7f3b7a03cb23146b2f7364183a
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-20 13:39:46 +0300

    added navigation

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { Provider } from "react-redux";
 import { store } from "./store";
-import HomeScreen from "./screens/HomeScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen, MapScreen } from "./screens/index";
+
+const screens = [
+  {
+    name: "HomeScreen",
+    component: HomeScreen,
+    options: { headerShown: false },
+  },
+  {
+    name: "MapScreen",
+    component: MapScreen,
+    options: { headerShown: false },
+  },
+];
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            {screens.map((screen, idx) => (
+              <Stack.Screen key={idx} {...screen} />
+            ))}
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
