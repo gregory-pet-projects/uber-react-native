fd8811475d8f7ff0a7a797350b01c53cd6c30b3e
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-20 15:50:03 +0300

    added search destination component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,24 @@ import { View, SafeAreaView, Image } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GOOGLE_MAPS_API_KEY } from "@env";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const onGoogleSearchPressHandler = (data, details = null) => {
+    dispatch(
+      setOrigin({
+        location: details.geometry.location,
+        description: data.description,
+      })
+    );
+
+    dispatch(setDestination(null));
+  };
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -11,6 +27,28 @@ const HomeScreen = () => {
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{
             uri: "https://Links.papareact.com/gzs",
+          }}
+        />
+        <GooglePlacesAutocomplete
+          nearbyPlacesAPI={"GooglePlacesSearch"}
+          debounce={400}
+          placeholder="Where From?"
+          minLength={2}
+          enablePoweredByContainer={false}
+          returnKeyType={"search"}
+          onPress={onGoogleSearchPressHandler}
+          fetchDetails={true}
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          query={{
+            key: GOOGLE_MAPS_API_KEY,
+            language: "en",
           }}
         />
         <NavOptions />
