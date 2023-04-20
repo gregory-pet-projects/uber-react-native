6bd880ac327c4a0792dd7cfb99882e59b48b807f
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-20 17:59:10 +0300

    changed keyboard view behavior

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen, MapScreen } from "./screens";
 import { renderScreens } from "./service/utils";
+import { KeyboardAvoidingView, Platform } from "react-native";
 
 const screens = [
   {
@@ -21,12 +22,19 @@ const screens = [
 
 export default function App() {
   const Stack = createNativeStackNavigator();
+  const isIOS = Platform.OS === "ios";
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <SafeAreaProvider>
-          <Stack.Navigator>{renderScreens(Stack, screens)}</Stack.Navigator>
-        </SafeAreaProvider>
+        <KeyboardAvoidingView
+          behavior={isIOS ? "padding" : "height"}
+          keyboardVerticalOffset={isIOS ? -64 : 0}
+          style={{ flex: 1 }}
+        >
+          <SafeAreaProvider>
+            <Stack.Navigator>{renderScreens(Stack, screens)}</Stack.Navigator>
+          </SafeAreaProvider>
+        </KeyboardAvoidingView>
       </NavigationContainer>
     </Provider>
   );
