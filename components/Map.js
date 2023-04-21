42e974dee1c8179fb928548af5098466ccfc1c21
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-21 15:12:30 +0300

    created logic to calculate trip price

--- components/Map.js
+++ components/Map.js
@@ -2,15 +2,20 @@ import MapView from "react-native-maps";
 
 import React, { useEffect, useRef } from "react";
 import tw from "tailwind-react-native-classnames";
-import { useSelector } from "react-redux";
-import { selectDestination, selectOrigin } from "../slices/navSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectDestination,
+  selectOrigin,
+  setTravelTimeInformation,
+} from "../slices/navSlice";
 import MapViewDirections from "react-native-maps-directions";
-import { GOOGLE_MAPS_API_KEY } from "@env";
+import { GOOGLE_MAPS_API_KEY, GOOGLE_API_URL } from "@env";
 import MarkerComponent from "./MarkerComponent";
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const dispatch = useDispatch();
 
   const mapRef = useRef();
 
@@ -20,6 +25,22 @@ const Map = () => {
     mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
       edgePadding: { top: 50, bottom: 50, right: 50, left: 50 },
     });
+  }, [origin, destination]);
+
+  useEffect(() => {
+    const getTravelTime = async () => {
+      const URL = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin.description}&destinations=${destination.description}&units=imperial&key=${GOOGLE_MAPS_API_KEY}`;
+
+      fetch(URL)
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    };
+    if (origin && destination) getTravelTime();
   }, [origin, destination]);
 
   return (
