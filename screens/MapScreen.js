47480055e2523bca71ab17c5287aa0b3bb0384f9
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-21 15:19:31 +0300

    added menu icon

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,11 @@
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { Map, NavigateCard, RideOptionsCard } from "../components";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { renderScreens } from "../service/utils";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 const screens = [
   {
     name: "NavigateCard",
@@ -18,9 +20,16 @@ const screens = [
 ];
 
 const MapScreen = () => {
+  const navigation = useNavigation();
   const Stack = createNativeStackNavigator();
   return (
     <View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("HomeScreen")}
+        style={tw`absolute top-16 right-8 bg-gray-100 p-3 rounded-full z-50`}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
